53b5ec44a49b742049e8010ab38e6d49986c9146
unrealuser1 <atharvaaswale121@gmail.com>
2024-01-11 02:08:46 +0530

    Initial commit

---
+++ app/src/main/java/com/unreal/saneforce/ui/DayReport.java
@@ -0,0 +1,16 @@
+package com.unreal.saneforce.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unreal.saneforce.R;
+
+public class DayReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_report);
+    }
+}
